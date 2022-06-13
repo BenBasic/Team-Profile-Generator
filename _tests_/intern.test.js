16bfb20e2206ba672f53c7d8267f58aed3159db4
BenBasic <benjaminmbasic@gmail.com>
2022-06-13 19:19:07 -0400

    Added describe answers and it functions with test information

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -3,6 +3,16 @@ const Intern = require("../lib/intern");
 describe("Intern", () => {
     describe("Answers", () => {
         it ("Should grab all the data from answers and return an object with properties")
-        
-    })
+        const testData = new Intern("Bobby", 123, "bhill@stricklandpropane.com", "Clown College");
+
+        expect(testData.name).toEqual("Bobby");
+        expect(testData.id).toEqual(123);
+        expect(testData.email).toEqual("bhill@stricklandpropane.com");
+        expect(testData.school).toEqual("Clown College");
+    });
+
+        it ("Should cause an error if no data is grabbed from the answers", () => {
+        const cb = () => new Intern();
+        expect(cb).toThrow();
+    });
 });
