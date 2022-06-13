3eced0bb6d231c7a1670bb4cfb6f96bd68abcc00
BenBasic <benjaminmbasic@gmail.com>
2022-06-13 19:35:33 -0400

    Added more it test conditions for functions such as getSchool, unsure as to why Im getting a Declaration or statement expected error, working on it currently

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -2,17 +2,27 @@ const Intern = require("../lib/intern");
 
 describe("Intern", () => {
     describe("Answers", () => {
-        it ("Should grab all the data from answers and return an object with properties")
-        const testData = new Intern("Bobby", 123, "bhill@stricklandpropane.com", "Clown College");
+        it ("Should grab all the data from answers and return an object with properties", () => {
 
-        expect(testData.name).toEqual("Bobby");
-        expect(testData.id).toEqual(123);
-        expect(testData.email).toEqual("bhill@stricklandpropane.com");
-        expect(testData.school).toEqual("Clown College");
-    });
+            const testData = new Intern("Bobby", 123, "bhill@stricklandpropane.com", "Clown College");
+
+            expect(testData.name).toEqual("Bobby");
+            expect(testData.id).toEqual(123);
+            expect(testData.email).toEqual("bhill@stricklandpropane.com");
+            expect(testData.school).toEqual("Clown College");
+        });
 
         it ("Should cause an error if no data is grabbed from the answers", () => {
-        const cb = () => new Intern();
-        expect(cb).toThrow();
+            const cb = () => new Intern();
+            expect(cb).toThrow();
+        });
+    });
+
+    describe("getSchool", () => {
+        it ("Should return the value of school in the data", () => {
+            const testData = new Intern("Bobby", 123, "bhill@stricklandpropane.com", "Clown College");
+
+            expect(testData.getSchool()).toEqual("Clown College");
+        });
     });
 });
