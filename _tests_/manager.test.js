ba5e9a2ec1a6f79571c28a9de126307507fbbd0c
BenBasic <benjaminmbasic@gmail.com>
2022-06-13 19:58:12 -0400

    Created manager.test.js, so far both test files are running fine and pass tests

---
+++ _tests_/manager.test.js
@@ -0,0 +1,31 @@
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+    describe("Answers", () => {
+        it ("Should grab all the data from answers and return an object with properties", () => {
+
+            const testData = new Manager("Buck", 555, "bstrickland@stricklandpropane.com", "(912) 748-4422");
+
+            expect(testData.name).toEqual("Buck");
+            expect(testData.id).toEqual(555);
+            expect(testData.email).toEqual("bstrickland@stricklandpropane.com");
+            expect(testData.officeNumber).toEqual("(912) 748-4422");
+        });
+    });
+
+    describe("getOfficeNumber", () => {
+        it ("Should return the value of officeNumber in the data", () => {
+            const testData = new Manager("Buck", 555, "bstrickland@stricklandpropane.com", "(912) 748-4422");
+
+            expect(testData.getOfficeNumber()).toEqual("(912) 748-4422");
+        });
+    });
+
+    describe("getRole", () => {
+        it ("Should return the value of the role assigned to Intern (which is Intern)", () => {
+            const testData = new Manager("Buck", 555, "bstrickland@stricklandpropane.com", "(912) 748-4422");
+
+            expect(testData.getRole()).toEqual("Manager");
+        });
+    });
+});
