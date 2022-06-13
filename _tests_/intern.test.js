1d2d166be8790c6497fa59e34bbd4abc65360577
BenBasic <benjaminmbasic@gmail.com>
2022-06-13 19:00:58 -0400

    Began setting up intern.test.js, looking into how to continue from here

---
+++ _tests_/intern.test.js
@@ -0,0 +1,8 @@
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+    describe("Answers", () => {
+        it ("Should grab all the data from answers and return an object with properties")
+        
+    })
+});
