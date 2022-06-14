0dab3662d2d7729b58382b4442dbb04d708c017c
BenBasic <benjaminmbasic@gmail.com>
2022-06-13 20:07:50 -0400

    Created engineer.test.js, ran into an odd error regarding octal literals when setting hank's id value to 001, it worked fine when changed to 111, Im going to look more into that but from what I can tell its irrelevant to the test code Im writing

---
+++ _tests_/engineer.test.js
@@ -0,0 +1,31 @@
+const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+    describe("Answers", () => {
+        it ("Should grab all the data from answers and return an object with properties", () => {
+
+            const testData = new Engineer("Hank", 111, "hhill@stricklandpropane.com", "BenBasic");
+
+            expect(testData.name).toEqual("Hank");
+            expect(testData.id).toEqual(111);
+            expect(testData.email).toEqual("hhill@stricklandpropane.com");
+            expect(testData.github).toEqual("BenBasic");
+        });
+    });
+
+    describe("getGitHub", () => {
+        it ("Should return the value of github in the data", () => {
+            const testData = new Engineer("Hank", 111, "hhill@stricklandpropane.com", "BenBasic");
+
+            expect(testData.getGitHub()).toEqual("https://github.com/BenBasic/");
+        });
+    });
+
+    describe("getRole", () => {
+        it ("Should return the value of the role assigned to Intern (which is Intern)", () => {
+            const testData = new Engineer("Hank", 111, "hhill@stricklandpropane.com", "BenBasic");
+
+            expect(testData.getRole()).toEqual("Engineer");
+        });
+    });
+});
